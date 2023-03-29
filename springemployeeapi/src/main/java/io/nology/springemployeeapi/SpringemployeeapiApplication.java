754f1f703503d665b7704c5b380fe2d13f679f31
mooreEffort <alexmaksimets@gmail.com>
2023-03-29 15:35:20 +1000

    frontend and backend initialised, working

---
+++ springemployeeapi/src/main/java/io/nology/springemployeeapi/SpringemployeeapiApplication.java
@@ -0,0 +1,13 @@
+package io.nology.springemployeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringemployeeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringemployeeapiApplication.class, args);
+	}
+
+}
---
+++ springemployeeapi/src/test/java/io/nology/springemployeeapi/SpringemployeeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.springemployeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringemployeeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
